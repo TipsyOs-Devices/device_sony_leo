3c8ed5a8f34fb9925ed17df6ad9f30e493aa1a33
Alx31 <alex31.couderc@gmail.com>
2015-07-26 23:13:40 +0200

    4.9 sabermod on rom is a bad idea :( keep it 4.8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,7 @@ KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-ea
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 
 # Rom Toolchain
-TARGET_GCC_VERSION_EXP := 4.9-sm
+TARGET_GCC_VERSION_EXP := 4.8-sm
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 25
