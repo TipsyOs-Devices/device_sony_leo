9090e0e04074509d0ecaea01e15f355d937b23b5
Alx31 <alex31.couderc@gmail.com>
2016-02-12 16:20:15 +0100

    Tispy bringup
    Change-Id: If8fdb0824bbb9406b09e0be835bd75ed73c8d8c7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,3 +29,17 @@ PRODUCT_VENDOR_KERNEL_HEADERS += device/sony/leo/kernel-headers
 BOARD_HARDWARE_CLASS += device/sony/leo/cmhw
 
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/clearpad/wakeup_gesture"
+
+# Kernel Toolchain
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-uber/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+
+# Rom Toolchain
+TARGET_GCC_VERSION_EXP := 4.9-uber
+
+# Optimizations
+CLANG_O3 := true
+STRICT_ALIASING := false
+KRAIT_TUNINGS := true
+GRAPHITE_OPTS := false
+ENABLE_GCCONLY := true
